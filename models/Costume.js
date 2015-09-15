c3c5c99fd5fb55cafa88927d9d99a16b32d536dd
MarthaMegarry <mega0015@umn.edu>
2015-09-15 08:47:41 -0500

    added user and costume schemas

--- models/Costume.js
+++ models/Costume.js
@@ -2,3 +2,23 @@
  * Created by Mothra on 9/14/15.
  */
 //this is where the Schema will go for the costumes
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var CostumeSchema = new Schema({
+    idLetter: { type: String, required: true },
+    idNumber: { type: Number, required: true },
+    catagory: { type: string, required: true },
+    gender: { type: String },
+    color: { type: String, required: true },
+    size: { type: String, required: true },
+    shoulders: { type: Number },
+    chest: { type: Number },
+    napeToHem: { type: NUmber },
+    description: { type: String },
+    isCheckedOut: { type: Boolean },
+    show: { type: Object },
+    cast: { type: Object }
+});
+
+module.exports = mongoose.model('Costume', CostumeSchema);
--- models/Users.js
+++ models/Users.js
@@ -1,3 +1,13 @@
 /**
  * Created by Mothra on 9/14/15.
  */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }, //plus all the security hashing stuffs
+    admin: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
